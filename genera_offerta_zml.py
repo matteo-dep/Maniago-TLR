75f44d69fc10290959905f4f0f9a41e8f4416388
APE FVG <matteo.depiccoli@ape.fvg.it>
2026-07-01 12:46:51 +0200

    Update genera_offerta_zml.py

--- genera_offerta_zml.py
+++ genera_offerta_zml.py
@@ -1,4 +1,15 @@
+# ==========================================
+# 1. RICHIAMO DELLE APP E LIBRERIE
+# ==========================================
 import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.graph_objects as go
+import plotly.express as px
+
+# ==========================================
+# IL TUO CODICE
+# ==========================================
 
 # ---------------------------------------------------------------------------
 # CALCOLI BILANCIO E GRAFICO 1: PROFILO ORARIO
